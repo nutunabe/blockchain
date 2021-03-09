06300d0075cbc71d8ebb987bfbe909a12bb0a403
Федоров Байытаан <47403995+nutunabe@users.noreply.github.com>
2021-03-09 15:01:28 +0900

    Update 1.sol

--- 1.sol
+++ 1.sol
@@ -1,63 +1,58 @@
-pragma solidity >= 0.4.22 < 0.6.0;
-
 contract Test
 {
-    enum RequestType { NewHome, EditHome }
-
-    struct Ownership
-    {
-        string homeAddress;
-        address owner;
-        uint p;
-    }
+    enum RequestType {NewHome, EditHome}
+    mapping(address => Employee) private employees;
+    mapping(address => Owner) private owners;
+    mapping(address => Request) private requests;
 
     struct Owner
     {
         string name;
-        uint passSer;
-        uint passNum;
-        string date; //TODO переделать
-        string phoneNumber;
+        int pass_ser;
+        int pass_num;
+        string phone_number;
+        uint256 pass_date;
     }
-
     struct Home
     {
-        string homeAddress;
-        uint area;
-        uint cost;
+        string Address;
+        fixed Area;
+        int Cost;
+        address owner;
     }
-
     struct Request
     {
         RequestType requestType;
         Home home;
-        uint result;
+        int result;
+        address adr;
     }
-
     struct Employee
     {
-        string name;
-        string position;
-        string phoneNumber;
+        string nameEmployee;
+        int position;
+        int phone_numberEmployee;
     }
 
-    mapping(address => Employee) private emplyees;
-    mapping(address => Owner) private owners;
-    mapping(address => Request) private requests;
-    mapping(string => Home) private homes;
-    mapping(string => Ownership[]) private ownerships;
+    Home private home;
+    Owner private owner;
+    Request private request;
+    Employee private employee;
 
-    function AddHome(string memory _adr, uint _area, uint _cost) public
+    string private message;
+
+    function Test1(string memory Message) public 
     {
-        Home memory h;
-        h.homeAddress = _adr;
-        h.area = _area;
-        h.cost = _cost;
-        homes[_adr] = h;
+        message = Message;
     }
 
-    function GetHome(string memory adr) public returns(uint _area, uint _cost)
+    function SetMessage(string memory newMessage) public
     {
-        return (homes[adr].area, homes[adr].cost);
+        message = newMessage;
+    }
+
+    function GetMessage() public returns (string memory)
+    {
+        return message;
     }
 }
