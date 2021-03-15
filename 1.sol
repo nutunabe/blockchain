a75f01f3b3a724a995478d75216bdaafef93896a
Федоров Байытаан <47403995+nutunabe@users.noreply.github.com>
2021-03-16 02:21:15 +0900

    Update 1.sol

--- 1.sol
+++ 1.sol
@@ -1,51 +1,61 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-contract Test
-{
+contract Test {
     enum RequestType {NewHome, EditHome}
+
     mapping(address => Employee) private employees;
     mapping(address => Owner) private owners;
     mapping(address => Request) private requests;
+    mapping(string => Home) private homes;
+    mapping(string => Ownership[]) private ownerships;
 
-    struct Owner
-    {
-        string name;
-        int pass_ser;
-        int pass_num;
-        string phone_number;
-        uint256 pass_date;
-    }
-    struct Home
-    {
-        string Address;
-        fixed Area;
-        int Cost;
+    struct Ownership {
+        string homeAddress;
         address owner;
+        uint256 p;
     }
-    struct Request
-    {
+
+    struct Owner {
+        string name;
+        uint256 passSer;
+        uint256 passNum;
+        string passDate;
+        string phoneNumber;
+    }
+
+    struct Home {
+        string homeAddress;
+        uint256 area;
+        uint256 cost;
+    }
+
+    struct Request {
         RequestType requestType;
         Home home;
-        int result;
-        address adr;
-    }
-    struct Employee
-    {
-        string nameEmployee;
-        int position;
-        int phone_numberEmployee;
+        uint256 result;
     }
 
-    function AddHome(string memory _adr, uint _area, uint _cost) public
-    {
-        Home memory h;
-        h.homeAddress = _adr;
-        h.area = _area;
-        h.cost = _cost;
-        homes[_adr] = h;
+    struct Employee {
+        string name;
+        string position;
+        string phoneNumber;
     }
-    
-    function GetHome(string memory adr) public returns(uint _area, uint _cost)
+
+    function AddHome(
+        string memory adr,
+        uint256 area,
+        uint256 cost
+    ) public {
+        Home memory h;
+        h.homeAddress = adr;
+        h.area = area;
+        h.cost = cost;
+        homes[adr] = h;
+    }
+
+    function GetHome(string memory adr)
+        public
+        returns (uint256 area, uint256 cost)
     {
         return (homes[adr].area, homes[adr].cost);
     }
